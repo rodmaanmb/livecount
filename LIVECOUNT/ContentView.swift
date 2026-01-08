c0197b25fab3d74f02926f4bf93dea6ba0da308f
Roman M'BARALI <robobara@icloud.com>
2026-01-08 13:52:39 +0100

    Initial Commit

---
+++ LIVECOUNT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LIVECOUNT
+//
+//  Created by Roman M'BARALI on 08/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LIVECOUNT/LIVECOUNTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIVECOUNTApp.swift
+//  LIVECOUNT
+//
+//  Created by Roman M'BARALI on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct LIVECOUNTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
